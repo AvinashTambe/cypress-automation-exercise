4ef40487f9d5d9d9987e75b63ac41faaa3b670f8
Avinash Tambe <avinashtambe@Avinashs-MacBook-Air.local>
2025-04-09 08:25:06 +0530

    updated code for Login and Signup page

--- cypress/e2e/signupPage.cy.ts
+++ cypress/e2e/signupPage.cy.ts
@@ -1,9 +1,11 @@
 import { SignUpPage } from "cypress/support/pages/SignUpPage";
 import { LoginPage } from "cypress/support/pages/LoginPage";
-import { BASE_URL } from "cypress/support/constants";
+import { BASE_URL, ROUTES } from "cypress/support/constants";
 
 
 describe('Signup without entering Name and Email', () => {
 
+    cy.visit('${BASE_URL}${ROUTES.SIGNUP}')
+    cy.url().should('eq', 'https://www.automationexercise.com/signup')
 
 });
